88ac3c3131e1e625889eadc26ac7b3d2baeefca2
chonjaekwan <153492004+chonjaekwan@users.noreply.github.com>
2023-12-22 17:29:20 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let depth1_li = document.querySelectorAll('header nav .depth1 > li')
+for(let i = 0; i < depth1_li.length; i++){
+    depth1_li[i].addEventListener('mouseover', function(){
+        // for(let i = 0; i < depth1_li.length; i++) {
+        //     depth1_li[i].classList.add('show');
+        // }
+        depth1_li.forEach(function(depth1) {
+            depth1.classList.add('show')
+        });
+        
+        console.log('완성');
+    });
+}
+for(let i = 0; i < depth1_li.length; i++){
+    depth1_li[i].addEventListener('mouseout', function(){
+        // for(let i = 0; i < depth1_li.length; i++) {
+        //     depth1_li[i].classList.add('show');
+        // }
+        depth1_li.forEach(function(depth1) {
+            depth1.classList.remove('show')
+        });
+        
+        console.log('완성');
+    });
+}
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+    faq.addEventListener("click", ()=>{
+        faq.classList.toggle("active");
+    });
+});
+
+
+let imgBox = document.querySelector('main .slider .slider-box');
+let img = document.querySelectorAll('.slider-box .title').length;
+let count = 0; 
+let imgWidth = -100; 
+
+setInterval(function(){
+    count++
+    if(count > img -1){count = 0};  
+    // console.log(count * imgWidth)
+    imgBox.style.transform = `translate(${count * imgWidth}vw, 0)`;
+}, 10000);
+
+let next = document.querySelector('main .main-banner .next-btn');
+let prev = document.querySelector('.prev-btn');
+
+next.addEventListener('click', function(){
+    img.style.transform = "translate(-100vw)"
+});
